eb537f85e1850a6b608d821cd21ed28599855ae7
ahsankhan <ak31111111@gmail.com>
2019-06-13 14:31:11 +0500

    TODO 1 completed

--- src/labTerminalB/Driver.java
+++ src/labTerminalB/Driver.java
@@ -7,11 +7,11 @@ public class Driver {
 	 * after you write regular expression the out put should be
 	 * The Good, the Bad, and the Ugly
 	 * after completing each TODO task commit and push to remote repository with commit message
-	 * 		"TODO 'TASK NUMBER' Complete" e.g. in this case "TODO 1 COMPLETED"
+	 * 		TODO 'TASK NUMBER' Complete" e.g. in this case "TODO 1 COMPLETED"
 	 */
 	public static void main(String[] args) {
 		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
-		String regex = "TODO 1";
+		String regex = "<[^>]*>";
 		String output = input.replaceAll(regex, "");
 		System.out.println(output);
 		
@@ -19,8 +19,10 @@ public class Driver {
 		Movie m1 = new Movie("Spancer", 0);
 		Movie m2 = new Movie("Speed", 1);
 		Movie m3 = new Movie("Blood Diamond",2);
+		
 		Customer me = new Customer("Nadir");
 		Customer me2 = new Customer("Allah Ditta");
+		
 		Rental r1 = new Rental(m1, 23);
 		Rental r2 = new Rental(m2,4);
 		Rental r3 = new Rental(m3,3);
